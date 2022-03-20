ee94260812735ddcf7c7d61e569a59fd841edb69
linye <linye102@163.com>
2022-03-20 16:53:51 +0800

    init push

---
+++ src/test/java/com/lincoln/lin/kafkastudy/KafkaStudyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lincoln.lin.kafkastudy;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KafkaStudyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
